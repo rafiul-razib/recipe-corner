c03f5325f5290470e69fd6f66acbf87b26b0a885
rafiul-razib <rafiul.rzb1@gmail.com>
2024-03-17 19:30:51 +0600

    navbar ok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import { CiSearch } from "react-icons/ci";
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
-            <div className="flex-1">
+            <div className="navbar flex justify-between bg-base-100">
+            <div>
             <div className="dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -29,11 +29,13 @@ const Navbar = () => {
             </div>
 
             <div className="flex-none gap-2">
-            
-            
                 <div className="form-control hidden lg:block">
-                <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto rounded-full" />
+                    <label className="input input-bordered rounded-full flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                    <input type="text" className="grow" placeholder="Search" />
+                    </label>
                 </div>
+                
                 <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar border-2">
                     <div className="w-fit text-4xl">
